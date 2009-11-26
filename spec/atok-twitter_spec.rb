884db77ad42a0fe56884a3e6277a71539566c3e3
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2009-11-26 18:49:00 +0900

    Added specs.

--- spec/atok-twitter_spec.rb
+++ spec/atok-twitter_spec.rb
@@ -1,7 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "AtokTwitter" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe 'AtokTwitter' do
+  describe 'extra' do
+    it 'has all required files' do
+      @extra_files.each do |file|
+        File.file?(File.join(@extra_root, file)).should be_true
+      end
+    end
+  end
+
+  describe 'plugin' do
+    it 'has all required files' do
+      @plugin_files.each do |file|
+        File.file?(File.join(@plugin_root, file)).should be_true
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,25 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'atok-twitter'
 require 'spec'
 require 'spec/autorun'
 
 Spec::Runner.configure do |config|
-  
+  config.before(:all) do
+    @extra_root  = File.join(File.dirname(__FILE__), '..', 'extra')
+    @extra_files = [
+      File.join('log', 'README'),
+      'README',
+      'speak_server.rb',
+      'start_servers.bat',
+      'tuple_server.rb',
+    ]
+    @plugin_root = File.join(File.dirname(__FILE__), '..', 'plugin')
+    @plugin_files = [
+      File.join('DATA', 'twitter.xml'),
+      File.join('DATA', 'twitter_wo_login_info.rb'),
+      'ATOK_PLUGIN_MESSAGE.DLL',
+      'SETUP.EXE',
+      'SETUPINFO.XML'
+    ]
+  end
 end
